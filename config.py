2146d20bb1aecec48a2d2c9cad67429052d619b9
Richard Rissanen <richardrissanen@me.com>
2017-08-23 15:24:31 -0500

    Added init logic for filtering by time

--- config.py
+++ config.py
@@ -1,12 +1,21 @@
 class App:
   __config = {
-    "events_url": "http://cal.nashvl.org/events.atom",
-    "listings_key": "entries",
-    "url_key": "link",
-    "title_key": "title",
-    "start_key": "start_time",
+    'events_url': 'http://cal.nashvl.org/events.atom',
+    'parser': {
+      'listings_key': 'entries',
+      'url_key': 'link',
+      'title_key': 'title',
+      'start_key': 'start_time',
+    },
+    'time': {
+      'timezone_offset': -5,
+      'end_of_day_offset': +15, # this assumes the script runs at 9am
+    }
   }
 
   @staticmethod
-  def config(key):
+  def config(key, key2=False):
+    if key2:
+      return App.__config[key][key2]
+    else:
       return App.__config[key]
--- main.py
+++ main.py
@@ -1,19 +1,35 @@
 import feedparser
+from datetime import datetime, timezone, timedelta, date
 
 from config import App
 
 # Grabs config
 events_url = App.config('events_url')
-listings_key = App.config('listings_key')
-url_key = App.config('url_key')
-title_key = App.config('title_key')
-start_key = App.config('start_key')
+
+listings_key = App.config('parser', 'listings_key')
+url_key = App.config('parser', 'url_key')
+start_key = App.config('parser', 'start_key')
+title_key = App.config('parser', 'title_key')
+
+timezone_offset = App.config('time', 'timezone_offset')
+end_of_day_offset = App.config('time', 'end_of_day_offset')
+
+delta = timedelta(hours=timezone_offset)
+tz = timezone(delta)
+current_time = datetime.now(tz)
+end_of_day = datetime.now(tz) + timedelta(hours=end_of_day_offset)
 
 # Parses Feed
 parsed_feed = feedparser.parse(events_url)
-prased_links = parsed_feed[listings_key]
+event_listings = parsed_feed[listings_key]
 
-# Prints Title, Url and start of first event
-print(prased_links[0][title_key])
-print(prased_links[0][url_key])
-print(prased_links[0][start_key])
+# Prints Title, Url and start of events for the current day
+for event in event_listings:
+  start = event[start_key].replace('T', '').replace('-05:00', '-0500').replace('-06:00', '-0600') # -06:00 I believe was bad data
+  start_time = datetime.strptime(start, "%Y-%m-%d%H:%M:%S%z")
+
+  if start_time > current_time and start_time < end_of_day:
+    print(event[title_key])
+    print(event[url_key])
+    print(start_time)
+    print(current_time)
