e61f258a08637e2aaa54db35cf93fa4c77baf0cb
Richard Rissanen <richardrissanen@me.com>
2017-08-24 15:28:59 -0500

    Added decorators in libs.py

--- libs.py
+++ libs.py
@@ -7,6 +7,7 @@ class Events:
   __url = App.config('events_url')
   __listings_key = App.config('parser', 'listings_key')
 
+  @staticmethod
   def get():
     parsed_feed = feedparser.parse(Events.__url)
     return parsed_feed[Events.__listings_key]
@@ -20,14 +21,17 @@ class Timer:
   __timezone_offset = App.config('time', 'timezone_offset')
   __end_of_day_offset = App.config('time', 'end_of_day_offset')
 
+  @staticmethod
   def current_time():
       tz = Timer.__timezone()
       return datetime.now(tz)
 
+  @staticmethod
   def end_of_day():
     tz = Timer.__timezone()
     return datetime.now(tz) + timedelta(hours=Timer.__end_of_day_offset)
 
+  @staticmethod
   def __timezone():
     delta = timedelta(hours=Timer.__timezone_offset)
     return timezone(delta)
@@ -43,6 +47,7 @@ class Twitter:
   __access_token = App.config('twitter', 'access_token')
   __access_token_secret = App.config('twitter', 'access_token_secret')
 
+  @staticmethod
   def post(message):
     auth = tweepy.OAuthHandler(Twitter.__consumer_key, Twitter.__consumer_secret)
     auth.set_access_token(Twitter.__access_token, Twitter.__access_token_secret)
