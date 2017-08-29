b824fb629102d43f79f935045f20cc60ecf9d660
Richard Rissanen <richardrissanen@me.com>
2017-08-29 11:51:51 -0500

    Removed config.sample in favor of config.py using env vars

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+import os
+
 class App:
   __config = {
     'events_url': 'http://cal.nashvl.org/events.atom',
@@ -12,10 +14,10 @@ class App:
       'end_of_day_offset': +15, # this assumes the script runs at 9am
     },
     'twitter': {
-      'consumer_key': '',
-      'consumer_secret': '',
-      'access_token': '',
-      'access_token_secret': '',
+      'consumer_key': os.environ['consumer_key'],
+      'consumer_secret': os.environ['consumer_secret'],
+      'access_token': os.environ['access_token'],
+      'access_token_secret': os.environ['access_token_secret'],
     },
   }
 
