bfab85be99f3478b0d24a5548a6a812b22f09d03
Richard Rissanen <richardrissanen@me.com>
2017-09-20 11:35:43 -0500

    Removed excess reference to old class

--- nashDevTwitterBot/twitter.py
+++ nashDevTwitterBot/twitter.py
@@ -10,8 +10,8 @@ def create_message(start_datetime, title, location, url):
   return start_datetime.strftime('%I:%M%p') + ' ' + title + ' at ' + location + ' ' + url
 
 def post(message):
-  auth = tweepy.OAuthHandler(Twitter.__consumer_key, Twitter.__consumer_secret)
-  auth.set_access_token(Twitter.__access_token, Twitter.__access_token_secret)
+  auth = tweepy.OAuthHandler(__consumer_key, __consumer_secret)
+  auth.set_access_token(__access_token, __access_token_secret)
 
   api = tweepy.API(auth)
 
